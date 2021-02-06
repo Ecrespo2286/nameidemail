d5907039373c84ab55f5181ef4ecf633c7d15d4a
Ecrespo2286 <78627069+Ecrespo2286@users.noreply.github.com>
2021-02-05 19:57:40 -0500

    Add files via upload

---
+++ projectone.java
@@ -0,0 +1,13 @@
+package firstproject;
+
+public class projectone {
+
+	public static void main(String[] args) {
+		String name ="Ernesto Crespo";
+		String email ="ecrespo@nyit.edu";
+		String id ="0427936";
+		System.out.println(name+"\n"+email+"\n"+id); 
+
+	}
+
+}
