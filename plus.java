2290969f428b44c70c5d7372bcf71a1d5e7abe9c
Ecrespo2286 <78627069+Ecrespo2286@users.noreply.github.com>
2021-02-05 20:04:11 -0500

    Add files via upload

---
+++ plus.java
@@ -0,0 +1,17 @@
+package sumofintegers;
+
+import java.util.Scanner;
+
+public class plus {
+
+	public static void main(String[] args) {
+	int sum,sum1;
+	System.out.println("Insert the number");
+	Scanner entrance = new Scanner(System.in);
+	sum =entrance.nextInt();
+	sum1=sum+1;
+	System.out.println("The sum is "+sum1);
+
+	}
+
+}
---
+++ sum.java
@@ -0,0 +1,14 @@
+package sumofintegers;
+
+public class sum {
+
+	public static void main(String[] args) {
+		int num1 = 1;
+		int num2 = 2;
+		int suma; 
+		suma =num1+num2;
+		System.out.println(suma); 
+
+	}
+
+}
