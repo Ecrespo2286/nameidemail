5119fea4da3ac8a7f868050e9c851df32407c0c3
Ecrespo2286 <78627069+Ecrespo2286@users.noreply.github.com>
2021-02-21 19:37:54 -0500

    Add files via upload

--- homework.java
+++ homework.java
@@ -1,41 +1,18 @@
-package homework4;
+package homework2;
 
 import java.util.Scanner;
 
 public class homework {
 
 	public static void main(String[] args) {
-		int day;
-		Scanner entrance= new Scanner(System.in);
-		System.out.print("Insert number of the week days");
-		day= entrance.nextInt();
-		switch(day) {
-		case 1:
-			System.out.print("The day is Monday");
-			break;
-		case 2:
-			System.out.print("The day is Tuesday");
-			break;
-		case 3:
-			System.out.print("The day is Wednesday");
-			break;
-		case 4:
-			System.out.print("The day is Thursday");
-			break;
-		case 5:
-			System.out.print("The day is Friday");
-			break;
-		case 6:
-			System.out.print("The day is Saturday");
-			break;
-		case 7:
-			System.out.print("The day is Sunday");
-			break;
-			default:System.out.print("The number of the day is not a day of the week");
-			break;
-		}
-		
-
+		int num;
+		Scanner enter=new Scanner(System.in);
+System.out.print("insert number");
+num = enter.nextInt();
+	if(num >0) {System.out.print("The number is positive");	
+	}
+	else {System.out.print("The number is negative");
+	
 	}
 
-}
+}}
